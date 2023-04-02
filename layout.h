d874f73e2da106c795cbb36e6ba1d007e4eddd10
frobiac <frobiac@gmail.com>
2023-04-02 10:54:57 +0200

    cosmetic

--- layout.h
+++ layout.h
@@ -363,10 +363,10 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
     _3a, _3b, L32, L33, L34, R30, R31, R32, _3y, _3z \
     ) \
     { \
-        { L00, L01, L02, L03, L04,           R00, R01, R02, R03, R04 }, \
-        { L10, L11, L12, L13, L14,           R10, R11, R12, R13, R14 }, \
-        { L20, L21, L22, L23, L24,           R20, R21, R22, R23, R24 }, \
-        { xxx, xxx, L32, L33, L34, R30, R31, R32                     } \
+        { L00, L01, L02, L03, L04,     R00, R01, R02, R03, R04 }, \
+        { L10, L11, L12, L13, L14,     R10, R11, R12, R13, R14 }, \
+        { L20, L21, L22, L23, L24,     R20, R21, R22, R23, R24 }, \
+        { xxx, xxx, L32, L33, L34,     R30, R31, R32           } \
     }
 
 
